d02a4f85383a4cb9919a526a71716f8625bf24d1
Mike Lanier <Mike.Lanier1@comcast.net>
2017-08-04 07:23:12 -0500

    add new task to the list

--- src/TODO.java
+++ src/TODO.java
@@ -13,3 +13,8 @@
 // TODO   [ ] Multiple layers.  Ex. Zork.  Layer for above ground paths and below ground paths
 // TODO   [ ] 3D display option for display which shows up/down more clearly
 // TODO       [ ] can OpenGL be called from Java and used for 3D display
+// TODO   [ ] Cell Info Dialog
+// TODO       [ ] Dismiss dialog on save and cancel
+// TODO       [ ] keyin name and have it display on the map
+// TODO       [ ] create a list of items found in the room
+// TODO   [ ] Need a panel for the player, show current location and what in his possession
